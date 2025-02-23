2657b0954f272c51e7d88913088064887f8e0e5e
brunojamelli <brunojamelli09@gmail.com>
2025-02-23 19:14:41 -0300

    feat: criação de classe utilitária

---
+++ lib/utils/variacao_utils.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class VariacaoUtils {
+  static Map<String, dynamic> getVariacaoStyle(double? variacao) {
+    if (variacao == null) {
+      return {
+        'color': Colors.grey, 
+        'symbol': '',
+      };
+    }
+
+    if (variacao >= 0) {
+      return {
+        'color': Colors.green, 
+        'symbol': '+',
+      };
+    } else {
+      return {
+        'color': Colors.red, 
+        'symbol': '-',
+      };
+    }
+  }
+}
